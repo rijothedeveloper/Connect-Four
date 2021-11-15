8c525b6cf9bcb25bc9336289fd981f1eee1362de
Rijo George <rijothedeveloper@gmail.com>
2021-11-14 19:38:39 -0800

    half way

--- connect4.js
+++ connect4.js
@@ -60,9 +60,9 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  for(let i=HEIGHT-1; i>=0; i--){
-    if(board[i][x] === null) {
-      return i;
+  for(let y=HEIGHT-1; y>=0; y--){
+    if(board[y][x] === null) {
+      return y;
     }
   }
   return null;
