6367c767c07e3083ae8e63ad9b43e7db39cd2ca5
Rijo George <rijothedeveloper@gmail.com>
2021-11-14 19:37:20 -0800

    half way

--- connect4.js
+++ connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,12 +17,20 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for(let i=0; i<HEIGHT; i++) {
+    const cell = [];
+    for (let j=0; j<WIDTH; j++) {
+      cell[j] = null;
+    }
+    board[i] = cell;
+   }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.getElementById("board");
 
   // TODO: add comment for this code
   var top = document.createElement("tr");
@@ -52,19 +60,36 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for(let i=HEIGHT-1; i>=0; i--){
+    if(board[i][x] === null) {
+      return i;
+    }
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  let cell = document.getElementById(`${y}-${x}`);
+  cell.className += "piece";
+  cell.className += " p"+currPlayer;
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  window.alert(msg);
+}
+
+// switch players 1 <=> 2
+function switchPlayers() {
+  if(currPlayer === 1) {
+    currPlayer = 2;
+  } else {
+    currPlayer = 1;
+  }
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -80,6 +105,7 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
+  board[y][x] = currPlayer;
   // TODO: add line to update in-memory board
   placeInTable(y, x);
 
@@ -92,7 +118,8 @@ function handleClick(evt) {
   // TODO: check if all cells in board are filled; if so call, call endGame
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  switchPlayers();
+  
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
